e49f7bbe191b97a6e0a29750d83aa5e698f8df79
Ana Almeida (CTW) <Ana.Almeida@ctw.bmwgroup.com>
2022-05-09 17:27:41 +0100

    comment file and add link

--- locustfile.py
+++ locustfile.py
@@ -4,4 +4,4 @@ class UdacityProject2(HttpUser):
     @task
     def enter_udacity_project(self):
         self.client.get("/")
-        self.client.get("/predict")
+        self.client.post("/predict")
